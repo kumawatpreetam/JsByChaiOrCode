1808a3f2d7ce5962a04b7732c3e2403577a2b9bd
kumawatpreetam <preetamkumawat0@gmail.com>
2026-02-07 21:04:41 +0530

    add some changes in type conversion file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -45,21 +45,19 @@ let str3 = str1 + str2;
 
 console.log(str3);
 
-
 console.log(1 + "2");
 console.log("1" + 2);
 console.log("1" + 2 + 2);
 console.log(1 + 2 + "2");
-console.log((3 + 4) * 5 % 3);
+console.log(((3 + 4) * 5) % 3);
 console.log(true);
 console.log(+true);
 console.log(+"");
 
+console.log("2" / "2");
 
+console.log("2" % "2");
 
+console.log("10" - 2);
 
-
-
-
-
-
+console.log("10" * "2");
