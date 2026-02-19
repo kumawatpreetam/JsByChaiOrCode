3c0a7e6234ecf606552b8d52dc222cc99312c93a
kumawatpreetam <preetamkumawat0@gmail.com>
2026-02-19 21:01:37 +0530

    AddObjectLearningOfDayThree

---
+++ 02_basics/05_objectdestructure.js
@@ -0,0 +1,46 @@
+const course = {
+  courseName: "js in hindi",
+  price: "999",
+  courseInstructor: "Preetam",
+  country: "India",
+};
+
+// course.courseInstructor;
+
+const { courseInstructor: Instructor } = course;
+
+const { country = "India" } = course;
+console.log(course);
+// console.log(country);
+
+// console.log(courseInstructor);
+
+// console.log(Instructor);
+
+// const navbar = ({ company }) => {};
+
+// navbar((company = "Preetam"));
+
+//Syntax of API,In what type of formate we can store data
+
+//TypeOne
+
+/*
+{
+    "name" : "Preetam",
+    "Coursename" : "js in hindi",
+    "price" : "free",
+}
+*/
+
+//TypeTwo
+
+[{}, {}, {}];
+
+// Tools to Study on APIs are as follow
+
+// JSONformatter
+
+let num = null;
+
+console.log(typeof num);
