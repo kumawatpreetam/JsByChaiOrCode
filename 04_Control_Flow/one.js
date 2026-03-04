7cf1fd7a6941d23d06d0347a35e2c915c67c9784
kumawatpreetam <preetamkumawat0@gmail.com>
2026-03-04 19:10:32 +0530

    Add Today's Js Learning

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,18 @@
+// if
+
+const isUserLoggedIn = true;
+if (3 != 2) {
+  console.log("Execuated");
+}
+
+// 2 === "2"
+//
+
+//<, >, <=, >=, ==, !=, === !==
+
+const score = 200;
+
+if (score > 100) {
+  const power = "fly";
+  console.log(`User Power : ${power}`);
+}
