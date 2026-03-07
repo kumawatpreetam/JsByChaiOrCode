06d626e50633843cc1716f8f604b94293d27a7dc
kumawatpreetam <preetamkumawat0@gmail.com>
2026-03-07 22:00:39 +0530

    Add Control Flow Statement

--- 04_Control_Flow/01_if-else.js
+++ 04_Control_Flow/01_if-else.js
@@ -29,31 +29,3 @@ const loggedInFromGoogle = false;
 if (userLogin && debitCard) {
   console.log("Allow to buy Course");
 }
-
-const month = 3;
-
-switch (month) {
-  case 1:
-    console.log("This is January Month");
-
-    break;
-  case 2:
-    console.log("This is Febraury Month");
-
-    break;
-
-  case 3:
-    console.log("This is March Month");
-
-    break;
-
-  case 4:
-    console.log("This is April Month");
-
-    break;
-
-  default:
-    console.log("Enter Valid Month number");
-
-    break;
-}
