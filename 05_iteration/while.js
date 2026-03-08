0b87b8453cd9275af541c50e8a85f684b660d19c
kumawatpreetam <preetamkumawat0@gmail.com>
2026-03-08 10:58:50 +0530

    Add While loop learning

---
+++ 05_iteration/while.js
@@ -0,0 +1,28 @@
+let index = 0;
+
+while (index <= 10) {
+  console.log(`Value of index is ${index}`);
+  index = index + 2;
+}
+
+let myArray = ["Flash", "Batman", "Superman"];
+
+let arr = 0;
+while (arr < myArray.length) {
+  console.log(`Value is ${myArray[arr]}`);
+  arr = arr + 1;
+}
+
+let score = 1;
+
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
+
+let score2 = 11;
+
+do {
+  console.log(`Score two is ${score2}`);
+  score2++;
+} while (score <= 10);
