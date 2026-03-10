a67753f9c28a12bd7879907ff9f584d78d0460ab
kumawatpreetam <preetamkumawat0@gmail.com>
2026-03-10 22:22:06 +0530

    Add Iteration Learning Day1 and Day2

---
+++ 05_iteration/forof.js
@@ -0,0 +1,37 @@
+// for of
+
+// ["","", "",""]
+//[{},{},{}]
+
+const arr = [1, 2, 3, 4]
+
+// for (const element of object) {
+
+// }
+
+// Here Object means on what thing/object you apply lopps to iterate like Array,String,Object etc.
+
+for (const element of arr) {
+    console.log(element);
+
+}
+
+const greeting = "Preetam Kumawat"
+
+for (const greet of greeting) {
+    console.log(`Each Character is ${greet}`);
+
+}
+
+// const myObj = {
+//     name: "Preetam",
+//     subject: "BCA"
+// }
+
+// for (const element of myObj) {
+//     console.log(element);
+
+// }
+
+// the for...of loop is not directly applicable to objects in JavaScript.
+
---
+++ 05_iteration/maps.js
@@ -0,0 +1,27 @@
+//Maps : It unique values always and it can not store duplicate values   
+
+const imap = new Map()
+
+imap.set('In', 'India')
+imap.set('USA', 'United States of America')
+imap.set('Fr', 'France')
+imap.set('In', 'India')
+
+// console.log(imap);
+
+for (const key of imap) {
+    console.log(key);
+
+}
+
+//If we want to print key and value differantly we can use the following syntax
+
+for (const [key, value] of imap) {
+    console.log(key, ":-", value);
+
+}
+
+for (const [value] of imap) {
+    console.log(":-", value);
+
+}
