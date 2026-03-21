bbf63ddfce23ae9f9352235f14750c6374fc1437
kumawatpreetam <preetamkumawat0@gmail.com>
2026-03-21 12:24:43 +0530

    Add DOM Day two Learning

--- 06_DOM/01_DOM-Intro.js
+++ 06_DOM/01_DOM-Intro.js
@@ -1,5 +1,20 @@
+//Run Whole code in Browser console
+
 console.log(window);
 
-console.log(window.document);
+console.log(window.document); //represents the entire HTML page loaded in the browser.
 
 console.dir(document);
+
+console.log(document.baseURI);
+
+console.log(document.links);
+
+console.log(document.links[2]);
+
+document.getElementById("first-heading");
+
+document.getElementById("first-heading").innerHTML;
+
+document.getElementById("first-heading").innerHTML =
+  "<h1>Kumawat Preetam </h1>";
