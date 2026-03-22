ed34a0eb9fba494ed7a4e83c7dc970f82ad09fdf
kumawatpreetam <preetamkumawat0@gmail.com>
2026-03-22 23:42:39 +0530

    Add Dom Learning of the day "

--- 06_DOM/01_DOM-Intro.js
+++ 06_DOM/01_DOM-Intro.js
@@ -12,9 +12,72 @@ console.log(document.links);
 
 console.log(document.links[2]);
 
-document.getElementById("first-heading");
+document.getElementById("firstHeading");
 
-document.getElementById("first-heading").innerHTML;
+document.getElementById("firstHeading").innerHTML;
 
-document.getElementById("first-heading").innerHTML =
-  "<h1>Kumawat Preetam </h1>";
+document.getElementById("firstHeading").innerHTML = "<h1>Kumawat Preetam </h1>";
+
+//   document.getElementById("first-heading").id
+
+// 'first-heading'
+// document.getElementById("first-heading").class
+
+// undefined
+// document.getElementById("first-heading").className
+
+// ''
+
+// document.getElementById("first-heading").getAttribute('id')
+
+// 'first-heading'
+// document.getElementById("first-heading").getAttribute('class')
+
+// null
+
+// document.getElementById("first-heading").setAttribute('class','test-1')
+
+// undefined
+
+// document.getElementById("first-heading").setAttribute('class','test-1 heading')
+
+// undefined
+
+// firstHeading.style.backgroundColor = "green";
+// ("green");
+
+// DIFFERANCE_BETWEEN_BELOW_THREE_METHOD
+
+// firstHeading.textContent;
+// ("\n        DOM Learning on ChaiAurCode\n        test text\n      ");
+
+// firstHeading.innerText;
+// ("DOM Learning on ChaiAurCode");
+
+// firstHeading.innerHTML;
+// ('\n        DOM Learning on ChaiAurCode\n        <span style="display: none">test text</span>\n      ');
+
+/* QuerieSelectors */
+
+document.querySelector("h2");
+
+document.querySelector("#firstHeading");
+
+document.querySelector(".heading");
+
+document.querySelector('input[type="password"]');
+
+document.querySelector("p:first-child");
+
+const myul = document.querySelector("ul");
+
+myul.querySelector("li");
+
+const listitem = document.querySelector("li");
+listitem.style.backgroundColor = "red";
+
+listitem.innerText;
+("one");
+
+listitem.innerText = "Preet";
+("Preet");
