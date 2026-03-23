4d08ee814d31412eb9a0b9cb4570229a9f3db369
kumawatpreetam <preetamkumawat0@gmail.com>
2026-03-23 21:14:31 +0530

    Add How to update text using DOM

--- 06_DOM/01_DOM-Intro.js
+++ 06_DOM/01_DOM-Intro.js
@@ -81,3 +81,41 @@ listitem.innerText;
 
 listitem.innerText = "Preet";
 ("Preet");
+
+//Node List and Html collection are not pure array
+
+/*
+document.querySelectorAll("li");
+NodeList(3) [li, li, li]
+const tempList = document.querySelectorAll("li");
+undefined
+tempList
+NodeList(3) [li, li, li]
+const arr = [1,2,3];
+undefined
+arr
+(3) [1, 2, 3]0: 11: 22: 3length: 3[[Prototype]]: Array(0)at: ƒ at()concat: ƒ concat()constructor: ƒ Array()copyWithin: ƒ copyWithin()entries: ƒ entries()every: ƒ every()fill: ƒ fill()filter: ƒ filter()find: ƒ find()findIndex: ƒ findIndex()findLast: ƒ findLast()findLastIndex: ƒ findLastIndex()flat: ƒ flat()flatMap: ƒ flatMap()forEach: ƒ forEach()includes: ƒ includes()indexOf: ƒ indexOf()join: ƒ join()keys: ƒ keys()lastIndexOf: ƒ lastIndexOf()length: 0map: ƒ map()pop: ƒ pop()push: ƒ push()reduce: ƒ reduce()reduceRight: ƒ reduceRight()reverse: ƒ reverse()shift: ƒ shift()slice: ƒ slice()some: ƒ some()sort: ƒ sort()splice: ƒ splice()toLocaleString: ƒ toLocaleString()toReversed: ƒ toReversed()toSorted: ƒ toSorted()toSpliced: ƒ toSpliced()toString: ƒ toString()unshift: ƒ unshift()values: ƒ values()with: ƒ with()Symbol(Symbol.iterator): ƒ values()Symbol(Symbol.unscopables): {at: true, copyWithin: true, entries: true, fill: true, find: true, …}[[Prototype]]: Object
+temp
+
+tempList[0].style.color = "green"
+'green'
+
+*/
+
+/*
+tempList.forEach((l)=>{
+    l.style.color = "red"
+})
+
+
+*/
+
+/*
+document.getElementsByClassName("list-item")
+HTMLCollection(4) [li.list-item, li.list-item, li.list-item, li.list-item]
+const tempList = document.getElementsByClassName("list-item")
+undefined
+Array.from(tempList)
+(4) [li.list-item, li.list-item, li.list-item, li.list-item]
+
+*/
