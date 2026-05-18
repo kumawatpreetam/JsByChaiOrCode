fff0fed610d092d21e4f07e8a70ba44685717519
Preetam Kunawat <preetamkumawat0@gmail.com>
2026-05-18 22:14:17 +0530

    Add Mon,May 18 Learning Code

--- 07_Events/one.js
+++ 07_Events/one.js
@@ -6,13 +6,13 @@
 // };
 
 //This is the Best approach of EventListener
-/*const val1 = document.getElementById("owl");
-num1.addEventListener("click", function () {
-  alert("owl was clciked");
-});
-*/
 
-const val1 = document.getElementById("owl");
+/*const val1 = document.getElementById("owl");
+val1.addEventListener("click", function () {
+  alert("owl was clciked");
+});*/
+
+/*const val1 = document.getElementById("owl");
 val1.addEventListener(
   "click",
   function (e) {
@@ -20,8 +20,83 @@ val1.addEventListener(
   },
   false,
 );
-
+*/
 //type,timestamp, defaultPrevented,target,toElement,srcElement,currentTarget,clientX,clientY,screenX,screenY
 //altkey,ctrlkey,shiftkey,keyCode
 
 //EventPropogation have two Context Event Bunbbling and Event capturing
+
+const imgClick = document.getElementById("images");
+const owlClick = document.getElementById("owl");
+
+/*imgClick.addEventListener(
+  "click",
+  (e) => {
+    console.log("click inside the ul");
+  },
+  false,
+);
+
+owlClick.addEventListener(
+  "click",
+  (e) => {
+    console.log("OwlClicked");
+    e.stopPropagation();
+  },
+  false,
+);
+*/
+/*
+imgClick.addEventListener(
+  "click",
+  (e) => {
+    console.log("click inside the ul");
+  },
+  true,
+);
+
+owlClick.addEventListener(
+  "click",
+  (e) => {
+    console.log("OwlClicked");
+  },
+  true,
+);
+*/
+
+const google = document.getElementById("google");
+
+google.addEventListener(
+  "click",
+  function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("Google was clicked");
+  },
+  false,
+);
+
+//e.target
+//e.target.parentNode
+
+/*
+document.querySelector("#images").addEventListener("click", function (e) {
+  console.log(e.target.parentNode);
+  let removeIt = e.target.parentNode;
+  removeIt.remove();
+  // removeIt.parentNode.removeChild(removeIt);
+});
+
+*/
+
+document.querySelector("#images").addEventListener("click", function (e) {
+  console.log(e.target.tagName);
+  if (e.target.tagName === "IMG") {
+    console.log(e.target.id);
+
+    let removeIt = e.target.parentNode;
+    removeIt.remove();
+  }
+
+  // removeIt.parentNode.removeChild(removeIt);
+});
